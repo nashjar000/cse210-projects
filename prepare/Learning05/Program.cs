724a05f01781f1206ad1f0b986d5c4944f59fc91
Jared Nash <nash_jar000@byui.edu>
2024-03-06 07:29:01 -0700

    Learning05 finished

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        // list to hold shapes
+        List<Shape> shapes = new List<Shape>();
+
+        // Create a square, set its color and side length, and add it to the list of shapes
+        Square s1 = new Square("Red", 3);
+        shapes.Add(s1);
+
+        // Create a rectangle, set its color, length, and width, and add it to the list of shapes
+        Rectangle s2 = new Rectangle("Blue", 4, 5);
+        shapes.Add(s2);
+
+        // Create a circle, set its color and radius, and add it to the list of shapes
+        Circle s3 = new Circle("Green", 6);
+        shapes.Add(s3);
+    
+        // Go through the list of shapes
+        foreach (Shape s in shapes)
+        {
+            // Get the color of the shape
+            string color = s.GetColor();
+
+            // Get the area of the shape
+            double area = s.GetArea();
+
+            // Print the color and area of the shape
+            Console.WriteLine($"The {color} shape has an area of {area}.");
+        }
     }
 }
---
+++ prepare/Learning05/shape.cs
@@ -0,0 +1,23 @@
+
+public abstract class Shape
+// the abstract function allows the class to be inherited
+{
+    private string _color; 
+
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
+    public string GetColor()
+    {
+        return _color;
+    }
+
+    public void SetColor(string color)
+    {
+        _color = color;
+    }
+
+    public abstract double GetArea();
+}
